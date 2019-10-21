17c18e0a03ec06d6004af7eabe2d91a4f67d16c4
jiyeonlab <jiyeonlab@gmail.com>
2019-10-21 22:24:46 +0900

    CollectionView 내에서 item drag하기
    drag하려는 item의 url을 가져와서, drop할 때 해당 위치에 url을 가진 image를 넣어준다.

--- ImageGallery/ImageGalleryViewController.swift
+++ ImageGallery/ImageGalleryViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ImageGalleryViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDropDelegate, UICollectionViewDelegateFlowLayout {
+class ImageGalleryViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDropDelegate, UICollectionViewDelegateFlowLayout, UICollectionViewDragDelegate {
     
     var images = [Image]()
     
@@ -17,6 +17,7 @@ class ImageGalleryViewController: UIViewController, UICollectionViewDataSource, 
             imageCollectionView.dataSource = self
             imageCollectionView.delegate = self
             imageCollectionView.dropDelegate = self
+            imageCollectionView.dragDelegate = self
         }
     }
     
@@ -38,59 +39,116 @@ class ImageGalleryViewController: UIViewController, UICollectionViewDataSource, 
     // MARK: UICollectionViewDelegateFlowLayout
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
-        let imageWidth: CGFloat = collectionView.bounds.size.width / 3.4
+//        let imageWidth: CGFloat = collectionView.bounds.size.width / 3.4
+//
+//        let imageHeight = imageWidth / images[indexPath.item].aspectRatio
+//
+//        return CGSize(width: imageWidth, height: imageHeight)
+        return CGSize(width: 200.0, height: 200.0)
+    }
+    
+    // MARK: UICollectionViewDragDelegate
+    func collectionView(_ collectionView: UICollectionView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+        print("drag delegate")
         
-        print("width : \(imageWidth)")
+        session.localContext = collectionView
         
-        let imageHeight = imageWidth / images[indexPath.item].aspectRatio
-        
-        return CGSize(width: imageWidth, height: imageHeight)
+        return dragItem(at: indexPath)
+    }
+    
+    private func dragItem(at indexPath: IndexPath) -> [UIDragItem] {
+        if let selectItem = (imageCollectionView.cellForItem(at: indexPath) as? ImageCollectionViewCell)?.imageURL {
+            let dragItem = UIDragItem(itemProvider: NSItemProvider(object: selectItem as NSItemProviderWriting))
+            
+            dragItem.localObject = selectItem
+            
+            return [dragItem]
+        }else{
+            return []
+        }
     }
     
     // MARK: UICollectionViewDropDelegate
     func collectionView(_ collectionView: UICollectionView, canHandle session: UIDropSession) -> Bool {
-        return session.canLoadObjects(ofClass: NSURL.self) && session.canLoadObjects(ofClass: UIImage.self)
+        let isSelf = ((session.localDragSession?.localContext) as? UICollectionView) == collectionView
+        
+        if isSelf {
+            print("url.self canload")
+            return session.canLoadObjects(ofClass: URL.self)
+        }else {
+            return session.canLoadObjects(ofClass: NSURL.self) && session.canLoadObjects(ofClass: UIImage.self)
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, dropSessionDidUpdate session: UIDropSession, withDestinationIndexPath destinationIndexPath: IndexPath?) -> UICollectionViewDropProposal {
-        return UICollectionViewDropProposal(operation: .copy, intent: .insertAtDestinationIndexPath)
+        let isSelf = ((session.localDragSession?.localContext) as? UICollectionView) == collectionView
+        
+        return UICollectionViewDropProposal(operation: isSelf ? .move : .copy, intent: .insertAtDestinationIndexPath)
+        //return UICollectionViewDropProposal(operation: .copy, intent: .insertAtDestinationIndexPath)
     }
+    
     func collectionView(_ collectionView: UICollectionView, performDropWith coordinator: UICollectionViewDropCoordinator) {
         let destinationIndexPath = coordinator.destinationIndexPath ?? IndexPath(item: images.count, section: 0)
         
         for item in coordinator.items {
-            let placeholderCell = coordinator.drop(item.dragItem, to: UICollectionViewDropPlaceholder(insertionIndexPath: destinationIndexPath, reuseIdentifier: "DropPlaceholderCell"))
             
-            var newImage = Image()
-            
-            // TASK 3번
-            item.dragItem.itemProvider.loadObject(ofClass: UIImage.self) { (provider, error) in
-                DispatchQueue.main.async {
-                    if let image = provider as? UIImage {
-                        print("Yes image")
-                        newImage.aspectRatio = image.size.width / image.size.height
-                        print("aspectRatio : \(image.size.width / image.size.height)")
-                    }else {
-                        placeholderCell.deletePlaceholder()
-                    }
-                }
-            }
-            
-            item.dragItem.itemProvider.loadObject(ofClass: NSURL.self) { (provider, error) in
-                DispatchQueue.main.async {
-                    if let url = provider as? URL {
-                        print("Yes url")
-                        newImage.url = url
+            // collectionView 내에서 움직일 때.
+            if let sourceIndexPath = item.sourceIndexPath
+            {
+                if let dragItemURL = (item.dragItem.localObject) as? URL {
+                    
+                    collectionView.performBatchUpdates({
+                        // 모델 먼저 업데이트.
+                        var newImage = Image()
+                        newImage.url = dragItemURL
+                        images.remove(at: sourceIndexPath.item)
+                        images.insert(newImage, at: destinationIndexPath.item)
                         
-                        // TASK 8번
-                        placeholderCell.commitInsertion { (insertionIndex) in
-                            self.images.insert(newImage, at: destinationIndexPath.item)
+                        // collectionView를 업데이트
+                        collectionView.deleteItems(at: [sourceIndexPath])
+                        collectionView.insertItems(at: [destinationIndexPath])
+                    })
+                    
+                    // drop animate
+                    coordinator.drop(item.dragItem, toItemAt: destinationIndexPath)
+                }
+            }
+            
+            // 앱 외부에서 앱으로 가져올 때.
+            else
+            {
+                let placeholderCell = coordinator.drop(item.dragItem, to: UICollectionViewDropPlaceholder(insertionIndexPath: destinationIndexPath, reuseIdentifier: "DropPlaceholderCell"))
+                
+                var newImage = Image()
+                
+                // TASK 3번
+                item.dragItem.itemProvider.loadObject(ofClass: UIImage.self) { (provider, error) in
+                    DispatchQueue.main.async {
+                        if let image = provider as? UIImage {
+                            //newImage.aspectRatio = image.size.width / image.size.height
+                            print("aspectRatio : \(image.size.width / image.size.height)")
+                        }else {
+                            placeholderCell.deletePlaceholder()
                         }
-                    }else{
-                        placeholderCell.deletePlaceholder()
+                    }
+                }
+                
+                item.dragItem.itemProvider.loadObject(ofClass: NSURL.self) { (provider, error) in
+                    DispatchQueue.main.async {
+                        if let url = provider as? URL {
+                            newImage.url = url
+                            
+                            // TASK 8번
+                            placeholderCell.commitInsertion { (insertionIndex) in
+                                self.images.insert(newImage, at: destinationIndexPath.item)
+                            }
+                        }else{
+                            placeholderCell.deletePlaceholder()
+                        }
                     }
                 }
             }
+            
         }
     }
     
