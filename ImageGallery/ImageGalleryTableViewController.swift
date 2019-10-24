5b75e3460b8665969327dd6ca46b428f2738091e
jiyeonlab <jiyeonlab@gmail.com>
2019-10-24 15:31:24 +0900

    Table View의 기능 수정
    섹션 0에서 삭제하여 섹션 1로 간 gallery들은 detail view에 뜰 수 없도록, shouldPerformSegue func를 추가함.
    섹션 0에서 선택하여 보여지고 있는 gallery를 삭제하면, 자신보다 앞 목록에 있는 gallery가 collection view로 뜨도록 함.

--- ImageGallery/ImageGalleryTableViewController.swift
+++ ImageGallery/ImageGalleryTableViewController.swift
@@ -21,7 +21,9 @@ class ImageGalleryTableViewController: UITableViewController{
     
     var deleteGalleryDocument = [ImageGallery]()
     
-    let sections = ["", "Recently Deleted"]
+    var sections = ["", ""]
+    
+    var uniqueNameNumber = 1
     
     // MARK: - Table view data source
     
@@ -59,15 +61,22 @@ class ImageGalleryTableViewController: UITableViewController{
     
     @IBAction func newImageGallery(_ sender: UIBarButtonItem) {
         let newGallery = ImageGallery()
-        newGallery.galleryName = "Untitled"
+        
+        newGallery.galleryName = "Untitled"+String(uniqueNameNumber)
+        //newGallery.galleryName = "Untitled".madeUnique(withRespectTo: imageGalleryDocument)
         imageGalleryDocument.append(newGallery)
         
-        print("newImageGallery 누른 후 Document count = \(imageGalleryDocument.count)")
+        uniqueNameNumber += 1
+        //print("newImageGallery 누른 후 Document count = \(imageGalleryDocument.count)")
         
-        //imageGalleryDocument += ["Untitled".madeUnique(withRespectTo: imageGalleryDocument)]
-        tableView.reloadData()
+        //tableView.reloadData()
+        tableView.beginUpdates()
+        let animateIndex = IndexPath(row: (self.imageGalleryDocument.count - 1), section: 0)
+        tableView.insertRows(at: [animateIndex], with: .fade)
+        tableView.endUpdates()
     }
     override func viewWillLayoutSubviews() {
+        //print("layout subview")
         super.viewWillLayoutSubviews()
         
         // master에 해당하는 table view가 항상 떠있는게 아니라, 스와이프하면 집어넣고, 다시 꺼내는 것도 가능하게 함.
@@ -86,31 +95,31 @@ class ImageGalleryTableViewController: UITableViewController{
     
     
     // Override to support editing the table view.
-//    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-//        if editingStyle == .delete {
-//            // Delete the row from the data source
-//
-//            if indexPath.section == 0 {
-//                print("0")
-//                let deleteTitle = imageGalleryDocument[indexPath.row].galleryName
-//
-//                imageGalleryDocument.remove(at: indexPath.row)
-//                tableView.deleteRows(at: [indexPath], with: .fade)
-//                deleteGalleryDocument += [deleteTitle]
-//                tableView.reloadData()
-//            }else {
-//                print("1")
-//                deleteGalleryDocument.remove(at: indexPath.row)
-//                tableView.deleteRows(at: [indexPath], with: .fade)
-//            }
-//
-//
-//
-//        } else if editingStyle == .insert {
-//            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-//
-//        }
-//    }
+    //    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+    //        if editingStyle == .delete {
+    //            // Delete the row from the data source
+    //
+    //            if indexPath.section == 0 {
+    //                print("0")
+    //                let deleteTitle = imageGalleryDocument[indexPath.row].galleryName
+    //
+    //                imageGalleryDocument.remove(at: indexPath.row)
+    //                tableView.deleteRows(at: [indexPath], with: .fade)
+    //                deleteGalleryDocument += [deleteTitle]
+    //                tableView.reloadData()
+    //            }else {
+    //                print("1")
+    //                deleteGalleryDocument.remove(at: indexPath.row)
+    //                tableView.deleteRows(at: [indexPath], with: .fade)
+    //            }
+    //
+    //
+    //
+    //        } else if editingStyle == .insert {
+    //            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    //
+    //        }
+    //    }
     
     
     /*
@@ -130,15 +139,12 @@ class ImageGalleryTableViewController: UITableViewController{
     
     
     // MARK: - Navigation
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("눌린 row index = \(indexPath.row)")
-        
-    }
-    
+
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
         if let galleryView = segue.destination.contents as? ImageGalleryViewController {
+            // 사용자가 section 0에서 보고자하는 gallery를 선택했을 때.
             if let selectGalleryName = (sender as? UITableViewCell)?.textLabel?.text {
                 galleryView.navigationItem.title = selectGalleryName
                 
@@ -147,11 +153,33 @@ class ImageGalleryTableViewController: UITableViewController{
                     galleryView.imageGallery = imageGalleryDocument[selectNumber]
                 }
             }
-           
-            
+            // 사용자가 section 0에서 gallery를 삭제하면 그 삭제된 gallery의 하나 앞의 gallery를 detail view에 보여주기 위한 코드.
+            else if let afterDeleteSetting = (sender as? ImageGallery) {
+                galleryView.navigationItem.title = afterDeleteSetting.galleryName
+                
+                galleryView.imageGallery = afterDeleteSetting
+            }
         }
+        
     }
     
+    // section 1에 있는 cell들은 segue 하는 것을 막기 위해 추가.
+    // 이 함수가 prepare(for segue, sender)보다 먼저 호출된다.
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        
+        if identifier == "SelectDocument" {
+            if let selectCell = (sender as? UITableViewCell) {
+                if let selectSection = self.tableView.indexPath(for: selectCell)?.section, selectSection == 1 {
+                    return false
+                }else {
+                    return true
+                }
+            }
+        }
+        
+        return false
+        
+    }
     // tableCell의 왼쪽에 삭제가 뜨도록.
     override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         if indexPath.section == 0 {
@@ -168,18 +196,39 @@ class ImageGalleryTableViewController: UITableViewController{
                 let animateIndex = IndexPath(row: (self.deleteGalleryDocument.count - 1), section: 1)
                 tableView.insertRows(at: [animateIndex], with: .fade)
                 tableView.endUpdates()
+                
+                // section 0에서 gallery를 삭제하면, 그 gallery가 detail view에 안뜨게 하기 위해 추가.
+                if self.imageGalleryDocument.count != 0 {
+                    if indexPath.row != 0 {
+                        self.performSegue(withIdentifier: "SelectDocument", sender: self.imageGalleryDocument[indexPath.row - 1])
+                    } else {
+                        self.performSegue(withIdentifier: "SelectDocument", sender: self.imageGalleryDocument[indexPath.row])
+                    }
+                }
+                
+                if self.deleteGalleryDocument.count == 1 {
+                    self.sections[1] = String("Recently Deleted")
+                    tableView.reloadSections([1], with: .none)
+                }
             }
             return UISwipeActionsConfiguration(actions: [deleteToRecentlyDelete])
         }else {
             // Recently Delete에서 영구 삭제하기.
             let deletePermanently = UIContextualAction(style: .destructive, title: "삭제") { (_, _, success) in
                 
-                //success(true)
-                self.deleteGalleryDocument.remove(at: indexPath.row)
-                self.tableView.deleteRows(at: [indexPath], with: .fade)
-                tableView.reloadData()
+                tableView.performBatchUpdates({
+                    self.deleteGalleryDocument.remove(at: indexPath.row)
+                    self.tableView.deleteRows(at: [indexPath], with: .fade)
+                    tableView.reloadData()
+                    
+                    if self.deleteGalleryDocument.count == 0 {
+                        self.sections[1] = String("")
+                        tableView.reloadSections([1], with: .none)
+                    }
+                })
+                
             }
-
+            
             return UISwipeActionsConfiguration(actions: [deletePermanently])
         }
     }
@@ -188,18 +237,25 @@ class ImageGalleryTableViewController: UITableViewController{
     override func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         if indexPath.section == 1 {
             let undelete = UIContextualAction(style: .normal, title: "복구") { (_, _, success) in
-                let recoverGallery = self.deleteGalleryDocument[indexPath.row]
                 
-                self.deleteGalleryDocument.remove(at: indexPath.row)
-                tableView.deleteRows(at: [indexPath], with: .fade)
-                
-                self.imageGalleryDocument.append(recoverGallery)
-                //tableView.reloadData()
-                tableView.beginUpdates()
-                let animateIndex = IndexPath(row: (self.imageGalleryDocument.count - 1), section: 0)
-                tableView.insertRows(at: [animateIndex], with: .fade)
-                tableView.endUpdates()
-                
+                tableView.performBatchUpdates({
+                    let recoverGallery = self.deleteGalleryDocument[indexPath.row]
+                    
+                    self.deleteGalleryDocument.remove(at: indexPath.row)
+                    tableView.deleteRows(at: [indexPath], with: .fade)
+                    
+                    self.imageGalleryDocument.append(recoverGallery)
+                    //tableView.reloadData()
+                    tableView.beginUpdates()
+                    let animateIndex = IndexPath(row: (self.imageGalleryDocument.count - 1), section: 0)
+                    tableView.insertRows(at: [animateIndex], with: .fade)
+                    tableView.endUpdates()
+                    
+                    if self.deleteGalleryDocument.count == 0 {
+                        self.sections[1] = String("")
+                        tableView.reloadSections([1], with: .none)
+                    }
+                })
             }
             return UISwipeActionsConfiguration(actions: [undelete])
         }else {
